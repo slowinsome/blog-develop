4ebec0640d48efabbfd24acddbd543e3978e4a90
slowinsome <slowinsome@gmail.com>
2022-10-22 23:41:35 +0900

    Add library wrapper for mdx

---
+++ src/lib/mdx.ts
@@ -0,0 +1,36 @@
+import { serialize } from "next-mdx-remote/serialize";
+import readingTime from "reading-time";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeCodeTitles from "rehype-code-titles";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrism from "rehype-prism-plus";
+
+// Referenced from:
+// https://github.com/leerob/leerob.io/blob/main/lib/mdx.ts
+export async function createMdxOutput(source: string) {
+  const mdxSource = await serialize(source, {
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeCodeTitles,
+        rehypePrism,
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: {
+              className: ["anchor"],
+            },
+          },
+        ],
+      ],
+      format: "mdx",
+    },
+  });
+
+  return {
+    html: mdxSource,
+    readingTime: readingTime(source).text,
+  };
+}
