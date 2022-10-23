6ca5b8b22bbbda0df6c86c6c1d7b8a0b5e270e4e
slowinsome <slowinsome@gmail.com>
2022-10-24 05:45:56 +0900

    Add type for mdx

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -1,14 +1,15 @@
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import readingTime from "reading-time";
-import remarkGfm from "remark-gfm";
-import rehypeSlug from "rehype-slug";
-import rehypeCodeTitles from "rehype-code-titles";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeCodeTitles from "rehype-code-titles";
 import rehypePrism from "rehype-prism-plus";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 // Referenced from:
 // https://github.com/leerob/leerob.io/blob/main/lib/mdx.ts
-export async function createMdxOutput(source: string) {
+export async function createMdxOutput(source: string): Promise<MdxOutput> {
   const mdxSource = await serialize(source, {
     mdxOptions: {
       remarkPlugins: [remarkGfm],
@@ -34,3 +35,8 @@ export async function createMdxOutput(source: string) {
     readingTime: readingTime(source).text,
   };
 }
+
+export type MdxOutput = {
+  html: MDXRemoteSerializeResult;
+  readingTime: string;
+};
