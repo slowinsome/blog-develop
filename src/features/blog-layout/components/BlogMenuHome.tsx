7a073f9e2b74554c41fe598fdd54f9c961366150
slowinsome <slowinsome@gmail.com>
2022-11-05 20:39:10 +0900

    Add link on Home menu

--- src/features/blog-layout/components/BlogMenuHome.tsx
+++ src/features/blog-layout/components/BlogMenuHome.tsx
@@ -1,16 +1,19 @@
 import { Button, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 export function BlogMenuHome() {
   return (
-    <Button variant="unstyled">
-      <Text
-        bgGradient="linear(to-tr, indigo, cyan)"
-        bgClip="text"
-        fontSize="xl"
-        fontWeight="bold"
-      >
-        slowinsome&apos;s blog
-      </Text>
-    </Button>
+    <Link href="/" passHref>
+      <Button variant="unstyled">
+        <Text
+          bgGradient="linear(to-tr, indigo, cyan)"
+          bgClip="text"
+          fontSize="xl"
+          fontWeight="bold"
+        >
+          slowinsome&apos;s blog
+        </Text>
+      </Button>
+    </Link>
   );
 }
