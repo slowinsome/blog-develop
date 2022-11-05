272bba7e872ceb3d2736f13b199d6a41ab0806b0
slowinsome <slowinsome@gmail.com>
2022-11-05 20:36:25 +0900

    Make Home menu to be Button

--- src/features/blog-layout/components/BlogMenuHome.tsx
+++ src/features/blog-layout/components/BlogMenuHome.tsx
@@ -1,14 +1,16 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 
 export function BlogMenuHome() {
   return (
-    <Text
-      bgGradient="linear(to-tr, indigo, cyan)"
-      bgClip="text"
-      fontSize="xl"
-      fontWeight="bold"
-    >
-      slowinsome&apos;s blog
-    </Text>
+    <Button variant="unstyled">
+      <Text
+        bgGradient="linear(to-tr, indigo, cyan)"
+        bgClip="text"
+        fontSize="xl"
+        fontWeight="bold"
+      >
+        slowinsome&apos;s blog
+      </Text>
+    </Button>
   );
 }
