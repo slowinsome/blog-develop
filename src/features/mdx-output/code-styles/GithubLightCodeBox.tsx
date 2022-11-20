90ce6d98596aeb0c472ec2e4312838d4145589f5
slowinsome <slowinsome@gmail.com>
2022-11-21 03:20:55 +0900

    Update themes

--- src/features/mdx-output/code-styles/GithubLightCodeBox.tsx
+++ src/features/mdx-output/code-styles/GithubLightCodeBox.tsx
@@ -1,4 +1,14 @@
 import styled from "@emotion/styled";
 import { LightThemeCodeBox } from "./LightThemeCodeBox";
 
-export const GithubLightCodeBox = styled(LightThemeCodeBox)``;
+export const GithubLightCodeBox = styled(LightThemeCodeBox)`
+  div[data-rehype-pretty-code-fragment] {
+    background-color: rgba(255 255 255 / 1);
+    border: 1px solid rgba(27 31 35 / 0.2);
+  }
+
+  div[data-rehype-pretty-code-title] {
+    background-color: rgba(27 31 35 / 0.1);
+    color: black;
+  }
+`;
--- src/features/mdx-output/code-styles/OneDarkProCodeBox.tsx
+++ src/features/mdx-output/code-styles/OneDarkProCodeBox.tsx
@@ -1,11 +1,8 @@
 import styled from "@emotion/styled";
-import { BaseCodeBox } from "./BaseCodeBox";
 import { DarkThemeCodeBox } from "./DarkThemeCodeBox";
 
 export const OneDarkProCodeBox = styled(DarkThemeCodeBox)`
-  div[data-rehype-pretty-code-fragment] pre[data-theme="light"],
-  div[data-rehype-pretty-code-fragment] code[data-theme="light"],
-  div[data-rehype-pretty-code-fragment] div[data-theme="light"] {
-    display: none;
+  div[data-rehype-pretty-code-fragment] {
+    background-color: rgb(255 255 255 / 0.1);
   }
 `;
